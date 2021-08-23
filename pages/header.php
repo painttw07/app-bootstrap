9721408a0590fefa400d357fa11abf9842cf86ab
Pâmela <painttw07@gmail.com>
2021-08-23 16:01:16 -0300

    commit 3

--- pages/header.php
+++ pages/header.php
@@ -11,22 +11,56 @@
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <style>@import url('https://fonts.googleapis.com/css2?family=Yomogi&display=swap');</style>
     <title>Alura - app bootstrap</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-
-        <nav class="navbar">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-nav" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon btn-navbar"></span>
-                </button>
-                <div class="collapse" id="collapse-nav">
-                        <ul class="navbar-nav mb-1">
-                            <li class="nav-item active"><button class="" href="#">Sing Up</button></li>
-                            <li class="nav-item"><a class="nav-link" aria-current="page" href="#about-us">About Us</a></li>
-                            <li class="nav-item"><a class="nav-link" aria-current="page" href="#contact-us">Contact</a></li>
-                            <li class="nav-item"><a class="nav-link" aria-current="page" href="#projects">Projects</a></li>
-                        </ul>
-                    </div>
-                </div>
+    <div class="mobile-container">
+        <div class="topnav">
+       <!-- <a href="#home" class="active">Logo</a><-->
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+            <div id="myLinks">
+                <a href="#">Home</a>
+                <a href="#about-us">Us</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact-us">Contact</a>
             </div>
-        </nav>
+        </div>
+    </div>
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myLinks");
+            if (x.style.display === "block") {
+                x.style.display = "none";
+            } else {
+                x.style.display = "block";
+            }
+        }
+    </script>
+</body>
+<!--<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="col-12">
+        <div class="col-sm-8 col-xs-8 col-lg-8"></div>
+        <div class="col-sm-4 col-xs-4 col-lg-4">
+            <ul class="navbar-nav">
+                <div class="col-xs-1 col-lg-1" style="margin:0px 10px;">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                </div>
+                <div class="col-xs-1 col-lg-1" style="margin:0px 10px;">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projects">Projects</a>
+                    </li>
+                </div>
+                <div class="col-xs-1 col-lg-1" style="margin:0px 10px;">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact-us">Contact</a>
+                    </li>
+                </div>
+            </ul>
+        </div>
+    </div>
+</nav>
+<-->
